696f367bd921a9799b7bcd8f0a5ee28f72fac9a3
uselessmeat <nonuniqusername@gmail.com>
2025-11-05 17:02:56 +0300

    up

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-var Undefine = {
-    "id": "UndefineId",
-    "title": "Undefine",
+// ----- Dorks -----
+var DorkMenu = {
+    "id": "DorkMenuId",
+    "title": "Dork",
     "contexts": ['selection']
 };
+var Text = {
+	"id": "TextDorkId",
+	"parentId": "DorkMenuId",
+	"title": "in text",
+	"contexts": ['selection']
+};
+var Url = {
+	"id": "UrlDorkId",
+	"parentId": "DorkMenuId",
+	"title": "in url",
+	"contexts": ['selection']
+};
+var Site = {
+	"id": "SiteDorkId",
+	"parentId": "DorkMenuId",
+	"title": "site",
+	"contexts": ['selection']
+};
 
+// ----- Addresses -----
 var AddressMenu = {
     "id": "AddressMenuId",
     "title": "Address",
@@ -58,15 +78,37 @@ var TokenSOL = {
     "contexts": ['selection']
 };
 
+// ----- Transactions -----
 var Transaction = {
     "id": "TransactionId",
     "title": "Transaction",
     "contexts": ['selection']
 };
 
+// ----- Signarures -----
+var SignatureMenu = {
+    "id": "SignatureMenuId",
+    "title": "Signature",
+    "contexts": ['selection']
+};
+var Function = {
+	"id": "FunctionId",
+	"parentId": "SignatureMenuId",
+	"title": "Function",
+	"contexts": ['selection']
+};
+var Event = {
+	"id": "EventId",
+	"parentId": "SignatureMenuId",
+	"title": "Event",
+	"contexts": ['selection']
+};
+
 
 chrome.runtime.onInstalled.addListener(() => {
-	chrome.contextMenus.create(Undefine)
+	chrome.contextMenus.create(DorkMenu)
+	chrome.contextMenus.create(Text)
+	chrome.contextMenus.create(Url)
 	
 	chrome.contextMenus.create(AddressMenu)
 	chrome.contextMenus.create(Account)
@@ -79,13 +121,24 @@ chrome.runtime.onInstalled.addListener(() => {
 	chrome.contextMenus.create(TokenSOL)
 	
 	chrome.contextMenus.create(Transaction)
+	
+	chrome.contextMenus.create(SignatureMenu)
+	chrome.contextMenus.create(Function)
+	chrome.contextMenus.create(Event)
+	chrome.contextMenus.create(Site)
 });
 
 
 chrome.contextMenus.onClicked.addListener(function(contextClick) {
 	if (contextClick.selectionText) {
-		if (contextClick.menuItemId == "UndefineId") {
+		if (contextClick.menuItemId == "TextDorkId") {
 			chrome.tabs.create({url:`https://www.google.com/search?q=intext:"${contextClick.selectionText}"`});
+		}
+		else if (contextClick.menuItemId == "UrlDorkId") {
+			chrome.tabs.create({url:`https://www.google.com/search?q=inurl:"${contextClick.selectionText}"`});
+		}
+		else if (contextClick.menuItemId == "SiteDorkId") {
+			chrome.tabs.create({url:`https://www.google.com/search?q=site:"${contextClick.selectionText}"`});
 		}
 		else if (contextClick.menuItemId == "AccountETHId") {
 			chrome.tabs.create({url:`https://debank.com/profile/${contextClick.selectionText}`});
@@ -112,6 +165,12 @@ chrome.contextMenus.onClicked.addListener(function(contextClick) {
 		else if (contextClick.menuItemId == "TransactionId") {
 			chrome.tabs.create({url:`https://phalcon.blocksec.com/tx/eth/${contextClick.selectionText}`});
 			chrome.tabs.create({url:`https://dashboard.tenderly.co/tx/mainnet/${contextClick.selectionText}`});
+		}
+		else if (contextClick.menuItemId == "FunctionId") {
+			chrome.tabs.create({url:`https://www.4byte.directory/signatures/?bytes4_signature=${contextClick.selectionText}`});
+		}
+		else if (contextClick.menuItemId == "EventId") {
+			chrome.tabs.create({url:`https://www.4byte.directory/event-signatures/?bytes_signature=${contextClick.selectionText}`});
 		}
 	}
 });	
